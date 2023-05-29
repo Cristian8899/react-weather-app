dbd89ae2eec2ae3afe745ce0a169f2f980b5c6a9
Cristian8899 <cristian.pitu88@gmail.com>
2023-05-29 14:20:21 +0300

    Version 1

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "7d1627a749mshbb55ad7b96cd488p1dc854jsnff6f8c0e6b4a",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "6694dfd16f0fb8b7503556fe663664a9"; // enter your key from openweather API
